4e351e81ab4de434af400af9b5bbed5d4a97cb36
TeamDman <TeamDman9201@gmail.com>
2022-12-23 12:06:05 -0500

    remove unused components

--- frontend/src/components/ErrorMessage.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    export let error: string | null | undefined;
-</script>
-
-{#if error && error != ""}
-    <div class="text-red-600 drop-shadow-md">
-        {error}
-    </div>
-{/if}
