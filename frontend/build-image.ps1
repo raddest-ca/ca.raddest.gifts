c8a66f58379a9d02cc11ff6522718728ed394ef0
TeamDman <TeamDman9201@gmail.com>
2022-12-23 11:14:29 -0500

    add containerization files

---
+++ frontend/build-image.ps1
@@ -0,0 +1,1 @@
+docker build . --tag teamdman.azurecr.io/gifts-frontend:latest
---
+++ frontend/push-image.ps1
@@ -0,0 +1,2 @@
+Write-Host "You might need to run `"az acr login --name teamdman`" first" -ForegroundColor "Cyan"
+docker push teamdman.azurecr.io/gifts-frontend:latest
